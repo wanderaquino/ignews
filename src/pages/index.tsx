b8cb3a994d9518134fd315247154e5d0ec341b92
Wander Aquino da Cruz <wander.aquino@gmail.com>
2021-07-25 00:09:20 -0300

    Structure from landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
 import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
     <Head>
-      <title>Início | ig.news</title>
+      <title>Home | ig.news</title>
     </Head>
-    <h1> Hello Word!</h1>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+      <span><img src="/images/palmas.png"></img> Hey, welcome</span>
+      <h1>News About the <span>React</span> world</h1>
+      <p>Get access to all the publications <br /> <span>for $ 9.90 month</span></p>
+      <button type="button">Subscribe now</button>
+      </section>
+      <img className={styles.girlCoding} src="/images/mulher.svg" alt="Girl coding" />
+    </main>
     </>
   )
 }
