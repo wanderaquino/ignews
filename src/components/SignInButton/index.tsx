8111177e7fc283bd81f628ddfa64fd1e55bd96b0
Wander Aquino <wander.aquino@gmail.com>
2021-07-27 23:15:00 -0300

    Displaying github provider data

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,19 @@
 import {FaGithub} from "react-icons/fa";
 import {FiX} from "react-icons/fi";
 import styles from "./styles.module.scss";
-import {signIn, useSession} from "next-auth/client";
+import {signOut, signIn, useSession, signin, signout} from "next-auth/client";
 
 export function SignInButton () {
     const [session] = useSession();
+    console.log(session);
     
     return (
-        <button className={styles.signInButton} type="button" onClick={() => signIn("github")}>
+        <button 
+            className={styles.signInButton} 
+            type="button" 
+            onClick={!session ? () => signin("github"): () => signout()}>
             <FaGithub color={session ? "#84d361" : "#eba417"}/>
-            {session ? "Wander Aquino da Cruz" : "Login with GitHub"}
+            {session ? session.user.name : "Login with GitHub"}
             {session && <FiX color="#737380" className={styles.closeIcon}/>}
         </button>
     )
