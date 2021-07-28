fe06c14997caeb44f373ed7e70add8444f25e0f8
Wander Aquino <wander.aquino@gmail.com>
2021-07-27 23:14:29 -0300

    ADding api for next auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import {AppProps} from "next/app";
 import "../styles/global.scss";
 import {Header} from "../components/Header"
+import {Provider as NextAuthProvider} from "next-auth/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
+      <NextAuthProvider session={pageProps.session}>
+        <Header />
+        <Component {...pageProps} />
+      </NextAuthProvider>
     </>
   );
 }
 
-export default MyApp
+export default MyApp;
