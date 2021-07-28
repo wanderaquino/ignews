ba6baa4cc39c332187d6c8e99d6bf2908712ba3b
Wander Aquino <wander.aquino@gmail.com>
2021-07-27 22:31:52 -0300

    Adding signin function and hook

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,16 @@
 import {FaGithub} from "react-icons/fa";
 import {FiX} from "react-icons/fi";
-
 import styles from "./styles.module.scss";
-
+import {signIn, useSession} from "next-auth/client";
 
 export function SignInButton () {
-    const isUserLogged = true;
+    const [session] = useSession();
     
     return (
-        <button className={styles.signInButton} type="button">
-            <FaGithub color={isUserLogged ? "#84d361" : "#eba417"}/>
-            {isUserLogged ? "Wander Aquino da Cruz" : "Login with GitHub"}
-            {isUserLogged && <FiX color="#737380" className={styles.closeIcon}/>}
+        <button className={styles.signInButton} type="button" onClick={() => signIn("github")}>
+            <FaGithub color={session ? "#84d361" : "#eba417"}/>
+            {session ? "Wander Aquino da Cruz" : "Login with GitHub"}
+            {session && <FiX color="#737380" className={styles.closeIcon}/>}
         </button>
     )
 }
