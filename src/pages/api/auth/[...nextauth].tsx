da8f4ffa3b4cb2fc3b4615c5b4a5752f2b0afdd9
Wander Aquino <wander.aquino@gmail.com>
2021-07-28 22:55:45 -0300

    Creating no duplicate user on faunadb

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import Providers from 'next-auth/providers';
+import {fauna} from "../../../services/fauna";
+import { query } from "faunadb";
+
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -9,6 +12,34 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET_ID,
       scope: "read:user"
     }),
-    // ...add more providers here
   ],
-})
+  jwt: {
+    signingKey: process.env.JWT_SIGNIN_KEY,
+  },
+  callbacks: {
+    async signIn (user, account, profile) {
+        try{
+          await fauna.query(
+            query.If(
+              query.Not(
+                query.Exists(
+                  query.Match(
+                      query.Index("users_email"), query.Casefold(user.email))
+                  )
+                ),
+                query.Create(
+                    query.Collection("users"),{data: {email: user.email}}),
+                query.Get(
+                  query.Match(
+                    query.Index("users_email"), query.Casefold(user.email))
+                )
+              )
+            )
+          return true;
+        } catch {
+          return false;
+        }
+      }
+    }
+  }
+)
