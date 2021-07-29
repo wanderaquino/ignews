9ced9b79ced18e3fba87e4da14c45c8967fa2e3e
Wander Aquino <wander.aquino@gmail.com>
2021-07-29 00:35:14 -0300

    handleSubscribeButton to stripe iframe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,13 +1,33 @@
 import styles from "./styles.module.scss";
-
+import {useSession, signIn} from "next-auth/client";
+import { getStripeJs } from "../../services/stripe.js";
 
 interface SubscribeButtonProps {
     priceId: string
 }
 
 export function SubscribeButton (subscribeProps: SubscribeButtonProps) {
+    const [session] = useSession();
+
+    async function handleSubscribe () {
+        if(!session) {
+            signIn("github");
+            return;
+        }
+        const response = await fetch("http://localhost:3000/api/createCheckoutSession", {
+            method: "POST",
+        }).then(response => response.json());
+
+        const stripe = await getStripeJs();
+        await stripe.redirectToCheckout({sessionId: response.sessionId.id});
+    }
+
     return (
-        <button type="button" className={styles.subscribeButton}>
+        <button 
+            type="button" 
+            className={styles.subscribeButton}
+            onClick={handleSubscribe}    
+        >
             
             Subscribe now
         
