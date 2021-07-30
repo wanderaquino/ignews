e3098d5d60d0e4e139e9f7592b55d45fa5dcc370
Wander Aquino <wander.aquino@gmail.com>
2021-07-29 23:04:47 -0300

    Avoiding duplicate client on stripe

--- src/pages/api/createCheckoutSession.ts
+++ src/pages/api/createCheckoutSession.ts
@@ -1,17 +1,55 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import {query} from "faunadb";
 import { stripe } from "../../services/stripe";
 import { getSession } from "next-auth/client";
+import {fauna} from "../../services/fauna";
+
+interface User {
+    ref : {
+        id: string
+    }
+    data: {
+        stripe_customer_id: string
+    }
+}
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
     if(request.method === "POST") {
+
         const session = await getSession({req: request});
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email
-        })
+
+        const user = await fauna.query<User>(
+            query.Get(
+                query.Match(
+                    query.Index("users_email"),
+                    query.Casefold(session.user.email)
+                )
+            )
+        );
+
+        let stripeCustomerId = user.data.stripe_customer_id;
+
+        if (!stripeCustomerId) {
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email
+            });
+            
+            await fauna.query(
+                query.Update(
+                    query.Ref(query.Collection("users"), user.ref.id), 
+                    {
+                        data: {stripe_customer_id: stripeCustomer.id }
+                    }
+                )
+            )
+            stripeCustomerId = stripeCustomer.id;
+        }
+
+
         const stripeCheckoutSession = 
         await stripe.checkout.sessions.create(
             {
-                customer: stripeCustomer.id,
+                customer: stripeCustomerId,
                 payment_method_types: ["card"],
                 billing_address_collection: "required",
                 line_items: [
