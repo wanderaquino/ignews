f9dfd6ca16b010da8a5819ca6f7303afca240775
Wander Aquino <wander.aquino@gmail.com>
2021-08-01 16:16:51 -0300

    Adding update for subscription that already exists

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -5,7 +5,11 @@ import { stripe } from "../../../services/stripe";
 export async function saveSubscription (
     subscriptionId: string,
     customerId: string,
+    createAction = false
 ) { 
+    console.log(subscriptionId);
+    console.log(createAction);
+
     const userStripeId = await fauna.query(
         query.Select(
             "ref",
@@ -18,14 +22,32 @@ export async function saveSubscription (
     const subscription = await stripe.subscriptions.retrieve(subscriptionId);
 
     const subscriptionData = {
-        id: subscriptionId,
+        id: subscription.id,
         userId: userStripeId,
         status: subscription.status,
         price_id: subscription.items.data[0].price.id
     };
-
-    await fauna.query(
-        query.Create(
-            query.Collection("subscriptions"), {data: subscriptionData})
-    )
+    
+    if(createAction) {
+        await fauna.query(
+            query.Create(
+                query.Collection("subscriptions"), {data: subscriptionData})
+        )
+    } else {
+        await fauna.query(
+            //Replace substitui a subscription por completo
+            query.Replace(
+                query.Select(
+                    "ref",  
+                    query.Get(
+                        query.Match(
+                            query.Index("subscription_by_id"),
+                            subscriptionData.id
+                        )
+                    )
+                ),
+                {data: subscriptionData}
+            )
+        )
+    }
 }
