85ef01b74b65eacd28f722fab24193abb44062ec
Wander Aquino <wander.aquino@gmail.com>
2021-08-01 19:11:26 -0300

    Fixing ident

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { GetStaticProps } from "next";
 import Head from "next/head";
 import {stripe} from "../services/stripe";
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -19,10 +18,10 @@ export default function Home(props: HomeProps) {
     </Head>
     <main className={styles.contentContainer}>
       <section className={styles.hero}>
-      <span><img src="/images/palmas.png"></img> Hey, welcome</span>
-      <h1>News About the <span>React</span> world</h1>
-      <p>Get access to all the publications <br /> <span>for {props.product.amount} month</span></p>
-      <SubscribeButton priceId={props.product.priceId}/>
+        <span><img src="/images/palmas.png"></img> Hey, welcome</span>
+        <h1>News About the <span>React</span> world</h1>
+        <p>Get access to all the publications <br /> <span>for {props.product.amount} month</span></p>
+        <SubscribeButton priceId={props.product.priceId}/>
       </section>
       <img className={styles.girlCoding} src="/images/mulher.svg" alt="Girl coding" />
     </main>
