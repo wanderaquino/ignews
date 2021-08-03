420ac4f3eb2981b8d439efb93fdb4026502aad9a
Wander Aquino <wander.aquino@gmail.com>
2021-08-02 21:22:50 -0300

    Routing to posts page after subscription

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.scss";
 import {useSession, signIn} from "next-auth/client";
 import { getStripeJs } from "../../services/stripe.js";
+import { useRouter } from "next/router";
 
 interface SubscribeButtonProps {
     priceId: string
@@ -8,15 +9,21 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton (subscribeProps: SubscribeButtonProps) {
     const [session] = useSession();
+    const router = useRouter();
 
     async function handleSubscribe () {
         if(!session) {
             signIn("github");
             return;
         }
+
+        if (session.activeSubscription) {
+            router.push("/posts");
+            return;
+        }
         const response = await fetch("http://localhost:3000/api/createCheckoutSession", {
             method: "POST",
-        }).then(response => response.json());
+        }).then(response => response.json()).catch(response => console.log(response));
 
         const stripe = await getStripeJs();
         await stripe.redirectToCheckout({sessionId: response.sessionId.id});
