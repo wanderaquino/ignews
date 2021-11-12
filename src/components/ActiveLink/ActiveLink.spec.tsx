0d80cbd8cfc8d2c6e29a1d7f51260c2356932383
Wander Aquino <wander.aquino@gmail.com>
2021-11-11 21:30:06 -0300

    Track test for activelink component

---
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -0,0 +1,38 @@
+import {render} from "@testing-library/react";
+import { ActiveLink } from ".";
+
+jest.mock("next/router", () => {
+    return {
+        useRouter() {
+            return {
+                asPath: "/"
+            }
+        }
+    }
+});
+
+
+describe("ActiveLink component", () => {
+
+    it("renders correctly", () => {
+        const {getByText} = render (
+            <ActiveLink href="/" activeClassName="active">
+                <a>Home</a>
+            </ActiveLink>
+        )
+    
+        expect(getByText("Home")).toBeInTheDocument;
+    });
+    
+    it("adds active classname to activeLink", () => {
+        const {getByText, debug} = render (
+            <ActiveLink href="/" activeClassName="active">
+                <a>Home</a>
+            </ActiveLink>
+        )
+    
+        debug();
+        expect(getByText("Home").classList.contains("active")).toBe(true);
+    });
+
+})
