4c51f5991031eb67b472975a2c74359997223341
Wander Aquino <wander.aquino@gmail.com>
2021-11-11 21:29:22 -0300

    Export jest config for test environment

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    testPathIgnorePatterns: ["/node_modules/", "/.next/"],
+    setuFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
+    transform: {"^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"},
+    testEnvironment: "jsdom"
+}
