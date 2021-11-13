37dd6402056ee8f9162dea84da1f9da0a26a5107
Wander Aquino <wander.aquino@gmail.com>
2021-11-13 18:14:00 -0300

    Track SignInButton component tests

---
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,43 @@
+import { render } from "@testing-library/react";
+import { mocked } from "ts-jest/utils";
+import { useSession } from "next-auth/client";
+import { SignInButton } from ".";
+
+jest.mock("next-auth/client", () => {
+    return {
+        useSession() {
+            return [null, false]
+        }
+    }
+});
+
+
+describe("Signin component", () => {
+
+    it("renders correctly when user is not authenticated", () => {
+        const mockedUseSession = mocked(useSession);
+        mockedUseSession.mockReturnValueOnce([null, false]);
+        const {getByText} = render (
+            <SignInButton />
+        )
+    
+        expect(getByText("SignIn with GitHub")).toBeInTheDocument();
+    });
+
+    it("renders correctly when user is authenticated", () => {
+        const mockedUseSession = mocked(useSession);
+        mockedUseSession.mockReturnValueOnce([{
+            user: {
+                name: "John Doe",
+                email: "john.doe@example.com",
+                image: "image.jpg"
+            },
+            expires: "fakeExpires"
+        }, false]);
+        const {getByText} = render (
+            <SignInButton />
+        )
+    
+        expect(getByText("John Doe")).toBeInTheDocument();
+    });
+})
